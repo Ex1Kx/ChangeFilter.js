0359fc3e290609773d4d169a3bb23626c732b0a4
Ex1Kx <thenewestekigoh@gmail.com>
2022-02-02 20:45:55 -0500

    [Ex1] Added The Code

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let start = 0;
+
+function high(){ 
+
+let value = document.getElementById('value').value = ++start; 
+}
+
+function low(){ 
+let value = document.getElementById('value').value = --start; 
+}
+function zero(){
+let value = document.getElementById('value').value = 0;
+}
